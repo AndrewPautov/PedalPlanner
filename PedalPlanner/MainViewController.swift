d177adcf4ca005fffe6e4da28d598e398a6e0390
AndrewPautov <apautov04@gmail.com>
2023-01-17 15:57:00 +0300

    Initial Commit

---
+++ PedalPlanner/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PedalPlanner
+//
+//  Created by Андрей Паутов on 17.01.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
